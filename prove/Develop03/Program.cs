2f7702f94b91adf898824bfe9c62299b3ef9938d
El-Benson <122732891+El-Benson@users.noreply.github.com>
2024-10-26 12:48:19 +0100

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,128 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 
-class Program
+namespace ScriptureMemorizer
 {
-    static void Main(string[] args)
+   
+    class Word
     {
-        Console.WriteLine("Hello Develop03 World!");
+        private string _text;
+        private bool _isHidden;
+
+        public Word(string text)
+        {
+            _text = text;
+            _isHidden = false;
+        }
+
+        public void Hide()
+        {
+            _isHidden = true;
+        }
+
+        public bool IsHidden()
+        {
+            return _isHidden;
+        }
+
+        public override string ToString()
+        {
+            return _isHidden ? "_____" : _text;
+        }
+    }
+
+    class ScriptureReference
+    {
+        public string Book { get; private set; }
+        public string VerseStart { get; private set; }
+        public string VerseEnd { get; private set; }
+
+        public ScriptureReference(string book, string verseStart, string verseEnd = "")
+        {
+            Book = book;
+            VerseStart = verseStart;
+            VerseEnd = verseEnd;
+        }
+
+        public override string ToString()
+        {
+            return string.IsNullOrEmpty(VerseEnd) ? $"{Book} {VerseStart}" : $"{Book} {VerseStart}-{VerseEnd}";
+        }
+    }
+
+    
+    class Scripture
+    {
+        private ScriptureReference _reference;
+        private List<Word> _words;
+        private Random _random;
+
+        public Scripture(ScriptureReference reference, string text)
+        {
+            _reference = reference;
+            _words = text.Split(' ').Select(w => new Word(w)).ToList();
+            _random = new Random();
+        }
+
+        public void Display()
+        {
+            Console.Clear();
+            Console.WriteLine(_reference);
+            foreach (var word in _words)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        public void HideRandomWords(int count)
+        {
+            int wordsToHide = Math.Min(count, _words.Count(w => !w.IsHidden()));
+            int hiddenWords = 0;
+
+            while (hiddenWords < wordsToHide)
+            {
+                int index = _random.Next(_words.Count);
+                if (!_words[index].IsHidden())
+                {
+                    _words[index].Hide();
+                    hiddenWords++;
+                }
+            }
+        }
+
+        public bool AllWordsHidden()
+        {
+            return _words.All(w => w.IsHidden());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var reference = new ScriptureReference("John", "3:16");
+            var scriptureText = "For God so loved the world that he gave his one and only Son, that whoever believes in him shall not perish but have eternal life.";
+            var scripture = new Scripture(reference, scriptureText);
+
+            
+            while (!scripture.AllWordsHidden())
+            {
+                scripture.Display();
+                Console.WriteLine("Press Enter to hide some words or type 'quit' to exit.");
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                scripture.HideRandomWords(3);
+            }
+
+            
+            Console.Clear();
+            Console.WriteLine("All words are now hidden. Well done!");
+        }
     }
 }
